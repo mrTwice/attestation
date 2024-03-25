ef1813a183647cf7e563f8659bd08b3d2def91c8
Igor Yampolskiy <clubber_77@mail.ru>
2024-03-26 00:30:59 +0300

    add javadoc info

--- project/TaskClient/src/main/java/ru/yampolskiy/taskclient/config/TaskClientConfiguration.java
+++ project/TaskClient/src/main/java/ru/yampolskiy/taskclient/config/TaskClientConfiguration.java
@@ -6,19 +6,35 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.JdkClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 
+/**
+ * Конфигурация клиента TaskClient.
+ */
 @Configuration
 public class TaskClientConfiguration {
 
+    /**
+     * Создает экземпляр RestClient.
+     * @param builder Builder для создания RestClient.
+     * @return Экземпляр RestClient.
+     */
     @Bean
     public RestClient restClient(RestClient.Builder builder) {
         return builder.build();
     }
 
+    /**
+     * Создает экземпляр RestClientCustomizer для настройки RestClient.
+     * @return Экземпляр RestClientCustomizer.
+     */
     @Bean
     public RestClientCustomizer restClientCustomizer() {
+        // Базовый URL для всех запросов
         String baseUrl = "http://localhost:8762/api/";
+        // Возвращает функцию для настройки RestClientBuilder
         return (restClientBuilder) -> restClientBuilder
+                // Использует JdkClientHttpRequestFactory для создания HTTP-запросов
                 .requestFactory(new JdkClientHttpRequestFactory())
+                // Устанавливает базовый URL для всех запросов
                 .baseUrl(baseUrl);
     }
 }
--- project/TaskClient/src/main/java/ru/yampolskiy/taskclient/models/user/UserPrincipal.java
+++ project/TaskClient/src/main/java/ru/yampolskiy/taskclient/models/user/UserPrincipal.java
@@ -6,16 +6,24 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 
+/**
+ * Класс UserPrincipal представляет принципала пользователя в системе Spring Security.
+ */
 public class UserPrincipal implements UserDetails {
 
     private User user;
 
+    /**
+     * Конструктор для создания экземпляра UserPrincipal на основе пользователя.
+     * @param user Пользователь, для которого создается принципал.
+     */
     public UserPrincipal(User user) {
         this.user = user;
     }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
+        // Преобразование ролей пользователя в объекты GrantedAuthority
         return user.getRoles()
                 .stream()
                 .map(role -> new SimpleGrantedAuthority(role.name())).toList();
@@ -36,21 +44,25 @@ public class UserPrincipal implements UserDetails {
 
     @Override
     public boolean isAccountNonExpired() {
+        // Аккаунт не истекает
         return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
+        // Аккаунт не заблокирован
         return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
+        // Учетные данные не истекли
         return true;
     }
 
     @Override
     public boolean isEnabled() {
+        // Аккаунт включен
         return true;
     }
 }
